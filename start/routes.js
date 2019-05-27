a5d37f500a087a98e6e95543cafba6ebfe3e40ae
Samuel Brehm <samuelbrehm@gmail.com>
2019-05-27 18:01:09 -0300

    Salvando relacionamentos

--- start/routes.js
+++ start/routes.js
@@ -6,27 +6,21 @@ const Route = use('Route')
 Route.post('users', 'UserController.store').validator('User')
 Route.post('sessions', 'SessionController.store').validator('Session')
 
-Route.post('passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
-Route.put('passwords', 'ForgotPasswordController.update').validator('ResetPassword')
+Route.post('passwords', 'ForgotPasswordController.store').validator(
+  'ForgotPassword'
+)
+Route.put('passwords', 'ForgotPasswordController.update').validator(
+  'ResetPassword'
+)
 
 Route.group(() => {
   Route.get('files/:id', 'FileController.show')
   Route.post('files', 'FileController.store')
 
-  Route.resource('projects', 'ProjectController').apiOnly().validator(new Map(
-    [
-      [
-        [ 'projects.store' ],
-        [ 'Project' ]
-      ]
-    ]
-  ))
-  Route.resource('projects.tasks', 'TaskController').apiOnly().validator(new Map(
-    [
-      [
-        [ 'projects.tasks.store' ],
-        [ 'Task' ]
-      ]
-    ]
-  ))
-}).middleware([ 'auth' ])
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['projects.store'], ['Project']]]))
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map([[['projects.tasks.store'], ['Task']]]))
+}).middleware(['auth'])
